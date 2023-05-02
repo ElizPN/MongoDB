1ed116d12319f47461e705697fd6b23aaa08f69f
ElizPN <pankevych.liza@gmail.com>
2023-05-02 11:32:06 +0200

    find just one document

--- index.js
+++ index.js
@@ -9,16 +9,27 @@ let mongoClient = new mongodb.MongoClient("mongodb://localhost:27017", {
 
 const db = mongoClient.db("initial-data");
 
-async function doStuff() {
+async function getOneDocument() {
   try {
     let coll = db.collection("prods");
-    let res = await coll.find({ cost: 100, rest: 10 }).toArray();
+    let res = await coll.findOne({ cost: 300, rest: 30 });
     console.log(res);
   } catch (error) {
     console.error(error);
   }
 }
-doStuff();
+getOneDocument();
+
+// async function doStuff() {
+//   try {
+//     let coll = db.collection("prods");
+//     let res = await coll.find({ cost: 100, rest: 10 }).toArray();
+//     console.log(res);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// doStuff();
 
 // connection to mongo
 
