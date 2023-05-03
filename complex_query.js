afe2a8fc3f0fb123567647ad2401bf86f56ccfc0
ElizPN <pankevych.liza@gmail.com>
2023-05-03 10:20:32 +0200

    apply $lt operator

---
+++ complex_query.js
@@ -0,0 +1,26 @@
+import mongodb from "mongodb";
+console.log("Before connectin...");
+
+let mongoClient = new mongodb.MongoClient("mongodb://localhost:27017", {
+  useUnifiedTopology: true,
+});
+
+const db = mongoClient.db("initial-data");
+
+async function getData() {
+  try {
+    let coll = db.collection("prods");
+    let cond = { cost: { $lt: 500 } };
+    let res = await coll.find(cond).toArray();
+    console.log("result", res);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();
+
+// Operators:
+
+// $lt - selects all documents where the given field is less than the passed value:  { cost: { $lt: 500 } }
+//
