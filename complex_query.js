094713cccf7b8feaa8698e408fb1a9aa50782748
ElizPN <pankevych.liza@gmail.com>
2023-05-03 11:18:43 +0200

    $eq  - equal the passed value

--- complex_query.js
+++ complex_query.js
@@ -12,8 +12,9 @@ async function getData() {
     let coll = db.collection("prods");
     let condLess = { cost: { $lt: 500 } };
     let condMore = { cost: { $gt: 500 } };
+    let equal = { cost: { $eq : 300} };
 
-    let res = await coll.find(condMore).toArray();
+    let res = await coll.find(equal).toArray();
     console.log("result", res);
   } catch (error) {
     console.error(error);
@@ -25,4 +26,7 @@ getData();
 // Operators:
 
 // $lt - selects all documents where the given field is less than the passed value:  { cost: { $lt: 500 } }
-//
+// $gt - given field is more than the passed value
+// $lte - less or equal the passed value
+// $gte - more or equal the passed value
+// $eq  - equal the passed value
