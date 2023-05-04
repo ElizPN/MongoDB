7523a4a83af07d9ea80da2f61eee4ea518bea9c5
ElizPN <pankevych.liza@gmail.com>
2023-05-04 15:03:21 +0200

    apply sort

--- sorting.js
+++ sorting.js
@@ -10,7 +10,7 @@ async function getUsers() {
   try {
     let coll = db.collection("users");
     let res = await coll.find().sort({ salary: 1, age: -1 }).toArray();
-    let res2 = await coll.find().skip(1).limit(3).toArray();
+    let res2 = await coll.find().sort({ age: -1 }).limit(5).toArray();
     console.log(res2);
   } catch (error) {
     console.error(error);
