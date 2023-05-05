1c04183e2cfc94568bcc2c91f9b43db37362d1c9
ElizPN <pankevych.liza@gmail.com>
2023-05-05 10:11:35 +0200

    get all users who have sizes between 3 and 5

---
+++ get_clothes.js
@@ -0,0 +1,26 @@
+import mongodb from "mongodb";
+
+let mongoClient = new mongodb.MongoClient("mongodb://localhost:27017", {
+  useUnifiedTopology: true,
+});
+
+const db = mongoClient.db("initial-data");
+
+async function getClothes() {
+  try {
+    const coll = db.collection("clothes");
+    const count = { colors: { $size: 3 } };
+    const count2 = {sizes: {$elemMatch: {$gt: 3, $lt: 5}}}
+    const res = await coll.find(count2).toArray();
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getClothes();
+
+mongoClient.connect(async function (error, mongo) {});
+
+// { color: { $size: 3 } } - amount of colors / $size
+//{sizes: {$elemMatch: {$gt: 3, $lt: 5}}} - arrange of sizes between 3 and 5 / $elemMatch
