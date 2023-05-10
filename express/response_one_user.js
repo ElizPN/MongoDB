8b69e7ebd3fff1e129892e3c0b87eea7ecc74124
ElizPN <pankevych.liza@gmail.com>
2023-05-10 16:01:31 +0200

    rename file

---
+++ express/response_one_user.js
@@ -0,0 +1,42 @@
+import expressHandlebars from "express-handlebars";
+import mongodb from "mongodb";
+import express from "express";
+import { __dirname } from "../__dirname.js";
+
+const app = express();
+
+let mongoClient = new mongodb.MongoClient("mongodb://localhost:27017", {
+  useUnifiedTopology: true,
+});
+
+const db = mongoClient.db("initial-data");
+
+const handlebars = expressHandlebars.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", handlebars.engine);
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+app.get("/user/show/:name", async (req, res) => {
+  const coll = db.collection("users");
+  const name = req.params.name;
+  const user = await coll.findOne({ name: name });
+  if (user) {
+    res.render("user", { user: user });
+  } else {
+    res.status(404).send("user not found");
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).render("error");
+});
+
+app.listen(3000, () => {
+  console.log("running one user");
+});
+
+// this code should be aware of enviroment , so we use __dirname
